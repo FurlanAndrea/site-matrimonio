b0da1a5889a0dd14ceebe3580089ba35d9bb4902
unknown <andrea.furlan20@gmail.com>
2025-10-20 13:13:01 +0200

    err.message

--- functions/submit.js
+++ functions/submit.js
@@ -89,7 +89,7 @@ export async function onRequestPost(context) {
       status: 200,
     });
   } catch (err) {
-    return new Response(JSON.stringify({ success: false, error: credentials }), {
+    return new Response(JSON.stringify({ success: false, error: err.message }), {
       headers: { "Content-Type": "application/json" },
       status: 500,
     });
