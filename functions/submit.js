c4905ca9f2530c61f2025a86ab461c2e45663ecf
unknown <andrea.furlan20@gmail.com>
2025-10-20 13:45:38 +0200

    try with credentials

--- functions/submit.js
+++ functions/submit.js
@@ -42,9 +42,8 @@ export async function onRequestPost(context) {
     const surname = data.get("surname");
     const message = data.get("message");
 
-    const clientEmail = context.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL;
-    const privateKey = context.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY;
-    const credentials = context.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS;
+    const clientEmail = JSON.parse(context.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS).client_email;
+    const privateKey = JSON.parse(context.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS).private_key;
 
     const header = { alg: "RS256", typ: "JWT" };
     const now = Math.floor(Date.now() / 1000);
